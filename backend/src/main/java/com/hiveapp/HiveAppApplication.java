97d0fe7b1b51bd581622fd67c3b4ac8f88a1ae6a
Mohamed Karroumi <karroumim2@gmail.com>
2026-02-22 21:55:08 +0000

    v2 initial setup

---
+++ backend/src/main/java/com/hiveapp/HiveAppApplication.java
@@ -0,0 +1,13 @@
+package com.hiveapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiveAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HiveAppApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/hiveapp/HiveAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hiveapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HiveAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
