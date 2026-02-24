5ef7bb621a3ee7d528e59d74f8ac75206254cc43
Mohamed Karroumi <karroumim2@gmail.com>
2026-02-24 22:23:19 +0000

    feat(account): add Account entity and repository for account management

---
+++ backend/src/main/java/com/hiveapp/platform/client/account/domain/repository/AccountRepository.java
@@ -0,0 +1,15 @@
+package com.hiveapp.platform.client.account.domain.repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.hiveapp.platform.client.account.domain.entity.Account;
+
+@Repository
+public interface AccountRepository extends JpaRepository<Account, UUID>{
+    Optional<Account> findByUserId(UUID userId);
+    boolean exisexistsByUserId(UUID userId);
+}
---
+++ backend/src/main/java/com/hiveapp/platform/client/account/package-info.java
@@ -0,0 +1,4 @@
+@org.springframework.modulith.ApplicationModule(
+    allowedDependencies = {"shared::security", "shared::event", "identity"}
+)
+package com.hiveapp.platform.client.account;
