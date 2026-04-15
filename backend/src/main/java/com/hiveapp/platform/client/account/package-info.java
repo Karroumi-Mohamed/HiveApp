3c2b2dd321039e21d614bcc6daf0801c448f0eb1
Mohamed Karroumi <mkaroumi123@gmail.com>
2026-04-16 00:51:58 +0100

    refactor: remove obsolete package-info files for account, collaboration, member, client, and role

--- backend/src/main/java/com/hiveapp/platform/client/account/package-info.java
+++
@@ -1,4 +0,0 @@
-@PermissionNode(key = "account", description = "Account Management")
-package com.hiveapp.platform.client.account;
-
-import dev.karroumi.permissionizer.PermissionNode;
--- backend/src/main/java/com/hiveapp/platform/client/member/package-info.java
+++
@@ -1,3 +0,0 @@
-@PermissionNode(key = "member", description = "Staff & Member Management")
-package com.hiveapp.platform.client.member;
-import dev.karroumi.permissionizer.PermissionNode;
