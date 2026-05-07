4d693c1cb0a12d535e05dcd0ecf9f983985966d3
Mohamed Karroumi <mkaroumi123@gmail.com>
2026-05-07 10:00:00 +0100

    exception: add custom security and state exceptions

---
+++ backend/src/main/java/com/hiveapp/shared/exception/InvalidRequestException.java
@@ -0,0 +1,11 @@
+package com.hiveapp.shared.exception;
+
+public class InvalidRequestException extends RuntimeException {
+    public InvalidRequestException(String message) {
+        super(message);
+    }
+
+    public InvalidRequestException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
